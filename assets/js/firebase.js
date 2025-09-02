d35bd27d67244386951cee6dd95d7926f08e5d8c
xplusyuz <xplusy.uz@gmail.com>
2025-09-02 20:24:53 +0500

    Update firebase.js

--- assets/js/firebase.js
+++ assets/js/firebase.js
@@ -2,12 +2,13 @@
 // 1) Firebase Console -> Web app -> SDK snippet (Compat)
 // 2) Replace the EXAMHOUSE_*** placeholders below
 const firebaseConfig = {
-  apiKey: "EXAMHOUSE_API_KEY",
-  authDomain: "EXAMHOUSE_PROJECT.firebaseapp.com",
-  projectId: "EXAMHOUSE_PROJECT",
-  storageBucket: "EXAMHOUSE_PROJECT.appspot.com",
-  messagingSenderId: "EXAMHOUSE_SENDER_ID",
-  appId: "EXAMHOUSE_APP_ID",
+  apiKey: "AIzaSyDYwHJou_9GqHZcf8XxtTByC51Z8un8rrM",
+  authDomain: "xplusy-760fa.firebaseapp.com",
+  projectId: "xplusy-760fa",
+  storageBucket: "xplusy-760fa.appspot.com",
+  messagingSenderId: "992512966017",
+  appId: "1:992512966017:web:5e919dbc9b8d8abcb43c80",
+  measurementId: "G-459PLJ7P7L"
 };
 
 // Initialize
