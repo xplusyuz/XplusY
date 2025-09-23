af671e3402af0ffda7c252fc07384854a313f52d
xplusyuz <xplusy.uz@gmail.com>
2025-09-23 20:26:54 +0500

    Update firebase-init.js

--- assets/js/firebase-init.js
+++ assets/js/firebase-init.js
@@ -1,4 +1,15 @@
-export const firebaseConfig={apiKey:"YOUR_API_KEY",authDomain:"YOUR_PROJECT.firebaseapp.com",projectId:"YOUR_PROJECT",storageBucket:"YOUR_PROJECT.appspot.com",messagingSenderId:"YOUR_SENDER_ID",appId:"YOUR_APP_ID"};
-export async function bootFirebase(){const[{initializeApp}]=await Promise.all([import("https://www.gstatic.com/firebasejs/10.12.4/firebase-app.js")]);const app=initializeApp(firebaseConfig);
-const[firestore,storage]=await Promise.all([import("https://www.gstatic.com/firebasejs/10.12.4/firebase-firestore.js"),import("https://www.gstatic.com/firebasejs/10.12.4/firebase-storage.js")]);
-const db=firestore.getFirestore(app);const st=storage.getStorage(app);window.db=db;window.storage=st;window.fs=firestore;window.st=storage;return{db,st,fs:firestore,stMod:storage};}
+export const firebaseConfig = {"apiKey":"AIzaSyDYwHJou_9GqHZcf8XxtTByC51Z8un8rrM","authDomain":"xplusy-760fa.firebaseapp.com","projectId":"xplusy-760fa","storageBucket":"xplusy-760fa.appspot.com","messagingSenderId":"992512966017","appId":"1:992512966017:web:5e919dbc9b8d8abcb43c80","measurementId":"G-459PLJ7P7L"};
+export async function bootFirebase(){
+  const [{ initializeApp }] = await Promise.all([
+    import("https://www.gstatic.com/firebasejs/10.12.4/firebase-app.js")
+  ]);
+  const app = initializeApp(firebaseConfig);
+  const [ firestore, storage ] = await Promise.all([
+    import("https://www.gstatic.com/firebasejs/10.12.4/firebase-firestore.js"),
+    import("https://www.gstatic.com/firebasejs/10.12.4/firebase-storage.js"),
+  ]);
+  const db = firestore.getFirestore(app);
+  const st = storage.getStorage(app);
+  window.db = db; window.storage = st; window.fs = firestore; window.st = storage;
+  return { db, st, fs: firestore, stMod: storage };
+}
