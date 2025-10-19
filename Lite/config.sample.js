83e42a6718fc42f87d364cbe07d80dcc1b66402b
xplusyuz <xplusy.uz@gmail.com>
2025-10-19 15:26:03 +0500

    1

---
+++ Lite/config.sample.js
@@ -0,0 +1,7 @@
+
+// Rename this file to config.js and edit with your Firebase credentials.
+export const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_AUTH_DOMAIN",
+  projectId: "YOUR_PROJECT_ID",
+};
