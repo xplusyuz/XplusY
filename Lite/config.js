a0c08d02f7ac8c8aa6fd55c41ffcf6c28fed0903
xplusyuz <xplusy.uz@gmail.com>
2025-10-19 15:33:07 +0500

    Update config.js

--- Lite/config.js
+++ Lite/config.js
@@ -1,5 +1,10 @@
 export const firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  projectId: ""
+  apiKey: "AIzaSyDYwHJou_9GqHZcf8XxtTByC51Z8un8rrM",
+  authDomain: "xplusy-760fa.firebaseapp.com",
+  projectId: "xplusy-760fa",
+  storageBucket: "xplusy-760fa.firebasestorage.app",
+  messagingSenderId: "992512966017",
+  appId: "1:992512966017:web:5e919dbc9b8d8abcb43c80",
+  measurementId: "G-459PLJ7P7L"
 };
+
