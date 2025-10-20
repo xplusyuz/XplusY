17e0a8d7a772696aa64f5cb05506f21aaa9d6fd0
xplusyuz <xplusy.uz@gmail.com>
2025-10-20 23:15:37 +0500

    1

--- Lite/service-worker.js
+++ Lite/service-worker.js
@@ -1,10 +1,5 @@
-const CACHE='lm-fb-v3_4_1';
-const CORE=['./','./index.html','./index.json','./manifest.webmanifest','./icons/icon-192.png','./icons/icon-512.png'];
-self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)))}); self.skipWaiting();
+const CACHE='lm-fb-v3_4_2';
+const CORE=['./','./index.html','./manifest.webmanifest'];
+self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE))); self.skipWaiting();});
 self.addEventListener('activate',e=>{self.clients && self.clients.claim && self.clients.claim(); e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>k!==CACHE&&caches.delete(k)))))});
-self.addEventListener('fetch',e=>{
-  const url=new URL(e.request.url);
-  if(url.origin===location.origin){
-    e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(res=>{caches.open(CACHE).then(c=>c.put(e.request,res.clone()));return res}).catch(()=>caches.match('./index.html'))));
-  }
-});
+self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(res=>{caches.open(CACHE).then(c=>c.put(e.request,res.clone()));return res}).catch(()=>caches.match('./index.html'))))});
