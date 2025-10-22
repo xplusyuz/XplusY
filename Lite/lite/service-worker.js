6720c4d69cbccf06adb0e3334f5ac26c78c5d694
xplusyuz <xplusy.uz@gmail.com>
2025-10-22 00:33:19 -0700

    1

--- Lite/lite/service-worker.js
+++
@@ -1,5 +0,0 @@
-const CACHE='lm-fb-v3_6_3';
-const CORE=['./','./index.html','./index.json','./manifest.webmanifest','./icons/icon-192.png','./icons/icon-512.png','./favicon.ico'];
-self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE))); self.skipWaiting();});
-self.addEventListener('activate',e=>{self.clients&&self.clients.claim&&self.clients.claim(); e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>k!==CACHE&&caches.delete(k)))))});
-self.addEventListener('fetch',e=>{const url=new URL(e.request.url); if(url.origin===location.origin){e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(res=>{caches.open(CACHE).then(c=>c.put(e.request,res.clone()));return res}).catch(()=>caches.match('./index.html'))));}});
