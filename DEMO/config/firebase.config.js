05fd23e29dca37af59e5a9e70956ae7c62f81636
xplusyuz <xplusy.uz@gmail.com>
2025-10-29 23:03:29 +0500

    1

--- DEMO/config/firebase.config.js
+++ DEMO/config/firebase.config.js
@@ -1,4 +1,9 @@
-export const firebaseConfig = {
+// firebase-config.js
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js';
+import { getAuth } from 'https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js';
+import { getFirestore } from 'https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js';
+
+const firebaseConfig = {
   apiKey: "AIzaSyDYwHJou_9GqHZcf8XxtTByC51Z8un8rrM",
   authDomain: "xplusy-760fa.firebaseapp.com",
   projectId: "xplusy-760fa",
@@ -7,3 +12,7 @@ export const firebaseConfig = {
   appId: "1:992512966017:web:5e919dbc9b8d8abcb43c80",
   measurementId: "G-459PLJ7P7L"
 };
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
