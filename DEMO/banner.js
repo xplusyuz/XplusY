7b1e427607c7f3b12fe1052838d410614bdfe1cd
xplusyuz <xplusy.uz@gmail.com>
2025-10-31 05:13:10 -0700

    Create banner.js

---
+++ DEMO/banner.js
@@ -0,0 +1,34 @@
+{
+  "sections": [
+    {
+      "title": "Asosiy reklama",
+      "banners": [
+        {
+          "image": "https://picsum.photos/1200/400?1",
+          "title": "🎉 Yangi Mavsum Boshlanmoqda!",
+          "desc": "LeaderMath Premium — 50% chegirma va sovg‘ali testlar"
+        },
+        {
+          "image": "https://picsum.photos/1200/400?2",
+          "title": "🔥 Online testlar endi yangi formatda!",
+          "desc": "Interaktiv, natijali va tezkor"
+        }
+      ]
+    },
+    {
+      "title": "Fanlar bo‘yicha",
+      "banners": [
+        {
+          "image": "https://picsum.photos/1200/400?3",
+          "title": "📘 Algebra 0 dan 1 gacha",
+          "desc": "To‘liq intensiv video kurs"
+        },
+        {
+          "image": "https://picsum.photos/1200/400?4",
+          "title": "⚡ Geometriya intensiv kurslari",
+          "desc": "Yangi mavzular va testlar bilan"
+        }
+      ]
+    }
+  ]
+}
