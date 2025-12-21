2e601a49483c16bf84d11a1ec3b76ddb7b8d7395
xplusyuz <xplusy.uz@gmail.com>
2025-12-21 21:49:06 +0500

    Update admin-config.js

--- admin-config.js
+++ admin-config.js
@@ -1,8 +1,8 @@
 // admin-config.js — Firebase web config shu yerga qo'yiladi
 // ⚠️ Bu secret emas. Firebase web config ochiq bo'lishi mumkin.
 window.FIREBASE_CONFIG = {
-  apiKey: "PASTE_ME",
-  authDomain: "PASTE_ME",
-  projectId: "PASTE_ME"
+   apiKey: "AIzaSyDYwHJou_9GqHZcf8XxtTByC51Z8un8rrM",
+  authDomain: "xplusy-760fa.firebaseapp.com",
+  projectId: "xplusy-760fa",
 };
 window.ADMIN_EMAILS = ["sohibjonmath@gmail.com"];
