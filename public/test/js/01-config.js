5eb89649709ee64a8df6ae3921944e195ff8b0d2
xplusyuz <xplusy.uz@gmail.com>
2026-01-25 09:27:02 +0500

    1

--- public/test/js/01-config.js
+++ public/test/js/01-config.js
@@ -37,13 +37,19 @@
             allowMultipleFormats: true,
         };
         
-        const FIREBASE_CONFIG = {
-          apiKey: "AIzaSyDYwHJou_9GqHZcf8XxtTByC51Z8un8rrM",
-          authDomain: "xplusy-760fa.firebaseapp.com",
-          databaseURL: "https://xplusy-760fa-default-rtdb.firebaseio.com",
-          projectId: "xplusy-760fa",
-          storageBucket: "xplusy-760fa.firebasestorage.app",
-          messagingSenderId: "992512966017",
-          appId: "1:992512966017:web:5e919dbc9b8d8abcb43c80",
-          measurementId: "G-459PLJ7P7L"
-        };
+       const CONFIG = {
+  useFirebase: true,
+  // for safety: avoid Firestore offline persistence on admin dashboards unless you explicitly want it
+  enableFirestorePersistence: false,
+};
+
+const FIREBASE_CONFIG = {
+  "apiKey": "AIzaSyDYwHJou_9GqHZcf8XxtTByC51Z8un8rrM",
+  "authDomain": "xplusy-760fa.firebaseapp.com",
+  "databaseURL": "https://xplusy-760fa-default-rtdb.firebaseio.com",
+  "projectId": "xplusy-760fa",
+  "storageBucket": "xplusy-760fa.firebasestorage.app",
+  "messagingSenderId": "992512966017",
+  "appId": "1:992512966017:web:5e919dbc9b8d8abcb43c80",
+  "measurementId": "G-459PLJ7P7L"
+};
