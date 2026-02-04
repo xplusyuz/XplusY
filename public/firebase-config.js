0e723c037e1d0916c56f39e202ef42f76f329fc2
xplusyuz <xplusy.uz@gmail.com>
2026-02-04 11:43:38 +0500

    Update firebase-config.js

--- public/firebase-config.js
+++ public/firebase-config.js
@@ -9,10 +9,14 @@ import { getStorage } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-
 
 // ⚠️ Firebase Console → Project settings → Your apps → Web app config'dan OLIB qo'ying.
 export const firebaseConfig = {
-  apiKey: "AIzaSyDYwHJou_9GqHZcf8XxtTByC51Z8un8rrM",
+   apiKey: "AIzaSyDYwHJou_9GqHZcf8XxtTByC51Z8un8rrM",
   authDomain: "xplusy-760fa.firebaseapp.com",
+  databaseURL: "https://xplusy-760fa-default-rtdb.firebaseio.com",
   projectId: "xplusy-760fa",
+  storageBucket: "xplusy-760fa.firebasestorage.app",
+  messagingSenderId: "992512966017",
   appId: "1:992512966017:web:5e919dbc9b8d8abcb43c80",
+  measurementId: "G-459PLJ7P7L"
 };
 
 function looksPlaceholder(v){
