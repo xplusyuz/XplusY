066757a90a56247f8be85cfc6b84e655e33a2fa6
xplusyuz <xplusy.uz@gmail.com>
2026-02-07 17:19:16 +0500

    1

---
+++ public/admin/firebase-config.js
@@ -0,0 +1,15 @@
+// Firebase config (TO'LDIRING)
+// Firebase Console -> Project settings -> Web app -> SDK config
+export const FIREBASE_CONFIG = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_PROJECT.firebaseapp.com",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_PROJECT.appspot.com",
+  messagingSenderId: "YOUR_SENDER_ID",
+  appId: "YOUR_APP_ID"
+};
+
+// Admin email allowlist (front-end). Firestore rules ham qo'ying!
+export const DEFAULT_ADMIN_EMAILS = [
+  "sohibjonmath@gmail.com"
+];
