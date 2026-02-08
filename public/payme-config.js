59d66cd8fec6826300cf850d62968878ccb6294c
xplusyuz <xplusy.uz@gmail.com>
2026-02-08 14:42:33 +0500

    Update payme-config.js

--- public/payme-config.js
+++ public/payme-config.js
@@ -1,7 +1,7 @@
 // Payme config (edit this file)
 // PAYME_MERCHANT_ID is shown in Payme Business cabinet (merchant / kassа settings).
 // Example format: "66f3c0c0..." (string)
-export const PAYME_MERCHANT_ID = "YOUR_PAYME_MERCHANT_ID";
+export const PAYME_MERCHANT_ID = "69885104803b08797f25c808";
 
 // Language sent to Payme checkout: uz / ru / en
 export const PAYME_LANG = "uz";
